aaf3ef8a78f31b3a1fc42183132059255c69890d
DFIT Ольга <Olga.Motovitsa@gmail.com>
2023-01-24 17:10:15 +0300

    add text

--- Lesson3/Ex1/Program.cs
+++ Lesson3/Ex1/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вид 1
+void Method1()
+{
+    System.Console.WriteLine("Афтор . . .");
+}
+Method1();
+
+
+
+
+
+
+
+
+
+
+
+// Вид 2
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+ // Method2(msg: "Текст сообщения");
+// Вид 2 (например несколько аргументов)
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method21("Текст", 4);
