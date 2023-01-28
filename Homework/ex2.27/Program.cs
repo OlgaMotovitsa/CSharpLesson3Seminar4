a5e21ba653a0e2de64cb74dea9bc9214471ff890
DFIT Ольга <Olga.Motovitsa@gmail.com>
2023-01-29 00:10:32 +0300

    add HomeWork

---
+++ Homework/ex2.27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+System.Console.WriteLine("Введите число");
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{num1} -> {GetSum(num1)}");
+
+static int GetSum(int num)
+{
+    int res = 0;
+    for(int i = 1; i <= num; i++)
+    {
+        res += i;
+    }
+    return res;
+}
